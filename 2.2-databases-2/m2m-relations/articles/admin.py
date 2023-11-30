629406d0ccb9aa8f3b7dfd124f3bae41eafbd570
Matveich1 <127345883+Matveich1@users.noreply.github.com>
2023-11-30 16:24:50 +0500

    Add files via upload

---
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+
+from .models import Article, Scope, Tag
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        var = 0
+        for form in self.forms:
+            if len(form.cleaned_data) == 0:
+                continue
+            if form.cleaned_data['is_main']:
+                var += 1
+        if var == 0:
+            raise ValidationError('Укажите основной раздел')
+        elif var > 1:
+            raise ValidationError('Основным может быть только один раздел')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class RelationshipInline(admin.TabularInline):
+    model = Scope
+    formset = RelationshipInlineFormset
+    extra = 2
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'text', 'published_at', 'image']
+    list_filter = ['published_at']
+    inlines = [RelationshipInline,]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
